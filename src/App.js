91264a2c6952aac4059bfd9bed5f69257fd443a9
vgiakoumakou <v.giakoumakou@ihu.edu.gr>
2020-12-12 16:10:03 +0200

    Add the Footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import NavBar from "./components/NavBar";
 import SearchBar from "./components/SearchBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <NavBar />
       <SearchBar />
+      <Footer />
     </>
   );
 }
